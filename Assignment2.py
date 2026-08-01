387593d14fa1f6dd964184aa56197e160b745d09
Tia759 <fgk@kent.ac.uk>
2026-08-01 15:43:04 +0000

    i have tested the random import with diffrent characters

--- Assignment2.py
+++ Assignment2.py
@@ -1,5 +1,3 @@
-import random
-coin = random.choice (["heads","tails"])
-print (coin)
+
 
 
---
+++ Testingimport.py
@@ -0,0 +1,3 @@
+import random
+character = random.choice (["mickey mouse","superman","Batman","Mario"])
+print (character)
