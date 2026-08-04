de19550c3adbac6297416773dee8c4d0ad923679
Tia759 <fgk@kent.ac.uk>
2026-08-04 13:41:49 +0000

    i have made it print a random colour and i have also added in a question for the user to answer which will help the computer decifer which colour they have gotten

--- Assignment2.py
+++ Assignment2.py
@@ -6,9 +6,11 @@ choice = input ("MENU\n 1.New Game\n 2.User Information\n 3.Previous Games Infor
 
 if choice := 1: 
  print ("Hello" , name) 
- print ("You will now receive a random colour. I am then going to ask you some questions.\nYour colour is...")
+ print ("You will now receive a random colour. I am then going to ask you some questions.Please type 1 for yes and 2 for no.\nYour colour is...")
  random_colour()
- Guess_one = input ("Is your colour a primary colour?")
+ Guess_one = input ("Is your colour a primary colour? 1=yes 2=no")
+ if Guess_one := 1 : 
+      
  
 
 else :
