551cd35f88ad04943ab029f34ff853b9ebee881e
Tia759 <fgk@kent.ac.uk>
2026-08-04 17:12:31 +0000

    i have made another if function in order to make the computer guess if the colour is red or not based on previous questions. If the answer is yes it prints out: Computer wins. If not it is supposed to print out the users name wins however it is not working right now.

--- Assignment2.py
+++ Assignment2.py
@@ -1,5 +1,7 @@
 from Testingimport import random_colour 
 
+colour = ["green","red","Brown","purple","pink"]
+
 name = input ("What is your name?")
 
 choice = input ("MENU\n 1.New Game\n 2.User Information\n 3.Previous Games Information\n")
@@ -9,8 +11,13 @@ if choice := 1:
  print ("You will now receive a random colour. I am then going to ask you some questions.Please type 1 for yes and 2 for no.\nYour colour is...")
  random_colour()
  Guess_one = input ("Is your colour a primary colour? 1=yes 2=no")
- if Guess_one := 1 : 
-      
+ if Guess_one := 2 : 
+     colour.pop(1)
+  
+     
+
+ print ("Is your colour associated with Nature?")
+  
  
 
 else :
--- Testingimport.py
+++ Testingimport.py
@@ -3,3 +3,12 @@ import random
 def random_colour():
  Colour = random.choice (["green","red","Brown","purple","pink"])
  print (Colour)
+
+def result(): 
+ Result = input ("Is your colour red? Yes = 1 No = 2")
+ if Result  := 1:
+      print ("Computer Win!")
+ else: 
+      print (name ,"Win!")
+
+result()
